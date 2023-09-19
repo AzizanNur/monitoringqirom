124795b0a25ba7769e7c129848c52d30105b7df9
azizan nur rohman <zhiz.nevermind@gmail.com>
2023-09-19 20:05:10 +0700

    save

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request){
+        return $user = User::create([
+            'name'      => $request->input('name'),
+            'email'     => $request->input('email'),
+            'password'  => Hash::make($request->input('password')),
+        ]);
+    }
+
+    public function user(){
+        return 'Authenticated User';
+    }
+}
