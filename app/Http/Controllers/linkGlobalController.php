087bde48e107644f4b559df059b20e5fa4b72fa4
azizan nur rohman <zhiz.nevermind@gmail.com>
2023-10-03 10:04:57 +0700

    crud simple

---
+++ app/Http/Controllers/linkGlobalController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\LinkGlobal;
+use Illuminate\Support\Facades\Validator;
+
+class linkGlobalController extends Controller
+{
+
+    function all_link(){
+        return LinkGlobal::where("is_status", 1)->get();
+    }
+
+    function find_one($id){
+        $data = LinkGlobal::where([
+                'id'=> $id,
+                'is_status' => 1
+            ]);
+        return response([
+            'data' => $data
+        ]);
+    }
+
+    function add_link(Request $request){
+       
+        $validator = Validator::make($request->input(), [
+            'name' => 'required|max:255',
+            'url'  => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response([
+                "message" => "failed"
+            ], 500);
+        }
+
+        LinkGlobal::create($request->all());
+
+        return response([
+            "message" => "success"
+        ]);
+
+    }
+
+    public function update(Request $request, $id){
+        try {
+            
+            $validator = Validator::make($request->input(), [
+                'name' => 'required|max:255',
+                'url'  => 'required',
+            ]);
+    
+            if ($validator->fails()) {
+                return response([
+                    "message" => "failed"
+                ], 500);
+            }
+            
+            LinkGlobal::where('id', $id)->update($request->all());
+            return response()->json([
+                'message' => 'update success'
+            ]);
+
+        } catch (Exception $error) {
+            // if error
+            return response()->json([
+                'message' => 'Something went Wrong!',
+                'error' => $error
+            ], 500);
+        }
+    }
+
+    public function delete($id){
+
+        LinkGlobal::where('id', $id)->update([
+            'is_status' => 0
+        ]);
+        return response([
+            'message' => 'delete success'
+        ]);
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\linkGlobalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +25,9 @@ Route::post('verify_token', [AuthController::class, 'verify_token']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
     Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::get('all_link', [linkGlobalController::class, 'all_link']);
+    Route::post('add_link', [linkGlobalController::class, 'add_link']);
+    Route::get('find_one/{id}', [linkGlobalController::class, 'find_one']);
+    Route::post('update/{id}', [linkGlobalController::class, 'update']);
+    Route::post('delete/{id}', [linkGlobalController::class, 'delete']);
 });
